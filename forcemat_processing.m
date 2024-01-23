dd52cb8cd3741ffd9e7a1b82794ae21c1c6b9767
Vaughn Chambers <vaughn@udel.edu>
2024-01-22 19:29:59 -0500

    First full commit (it works nows)

--- forcemat_processing.m
+++ forcemat_processing.m
@@ -1,80 +1,94 @@
-function [fm_data_processed,lhs,rhs,lto,rto]= forcemat_processing(fm_data,lank_x,lank_y,lhee_x,lhee_y,ltoe_x,ltoe_y,rank_x,rank_y,rhee_x,rhee_y,rtoe_x,rtoe_y)
-% [fm_data_processed,lhs,rhs,lto,rto]= forcemat_processing(fm_data,lank_x,lank_y,
-%          lhee_x,lhee_y,ltoe_x,ltoe_y,rank_x,rank_y,rhee_x,rhee_y,rtoe_x,rtoe_y)
+function [fm_data_processed,fm_parameters]= forcemat_processing(fm_data,lank_x,lank_y,lhee_x,lhee_y,ltoe_x,ltoe_y,rank_x,rank_y,rhee_x,rhee_y,rtoe_x,rtoe_y)
+% [fm_data_processed,fm_parameters]= 
+%  forcemat_processing(fm_data,lank_x,lank_y,lhee_x,lhee_y,ltoe_x,ltoe_y,rank_x,rank_y,rhee_x,rhee_y,rtoe_x,rtoe_y)
 %
-% Inputs should already be truncated, but fm_data doesn't have to be the
-% same length as VICON data
+% VICON data should already be truncated and resampled at the same sampling
+% rate (up-sampled or down-sampled)
 
-% Constants
-frame_total= length(lank_x);
-
-% Upsample Forcemat Data
-fm_time= fm_data(:,1)-fm_data(1,1);
-t_end= frame_total*1000;
-ind= find(min(abs(fm_time - t_end)) == abs(fm_time - t_end)); % Find closest value to end
-fm_data_truncated= fm_data(1:ind,5:end);
-fm_time_truncated= fm_time(1:ind);
-fm_data_upsampled= interp1(fm_time_truncated,fm_data_truncated,linspace(0,fm_time_truncated(end),frame_total));
+% Check sampling of VICON & forcemat data
+if length(lank_x) ~= length(fm_data)
+    error('VICON data and forcemat data are not the same length. Resample them.')
+else
+    frame_total= length(lank_x);
+end
 
 % Reshape Forcemats into grid
 for i= 1:frame_total
-    fm_grid{i,1}= reshape(fm_data_upsampled(i,:),96,88);
+    fm_grid{i,1}= reshape(fm_data(i,:),96,88);
     fm_grid_left{i,1}= fm_grid{i}(:,1:44);
     fm_grid_right{i,1}= fm_grid{i}(:,45:88);
 end
 
-% Find "force" on left & right belts
+% Find "force" on left & right belts (not the final force value)
 for i= 1:frame_total
     force_left(i,1)= sum(fm_grid_left{i},'all');
     force_right(i,1)= sum(fm_grid_right{i},'all');
 end
 
-%%%%%%%% STOP HERE %%%%%%%%%%
-% Heel strike & toe off using Forcemats 
-clear ind lhs_fm
-signal= smooth(force_left,10);
-[valleys(:,1), ind(:,1)]= findpeaks(-signal,'MinPeakHeight',-350,'MinPeakWidth',0.65,'MinPeakDistance',100);
-valleys= -valleys;
-[peaks(:,1),~]= findpeaks(signal,'MinPeakHeight',3000,'MinPeakWidth',0.65,'MinPeakDistance',100);
-threshold= (mean(peaks)/1.1-mean(valleys))*.05 + mean(valleys); % finding 5% threshold
+% Left heel strike & toe off using forcemats 
+clear ind peaks valleys
+signal= smooth(force_left,10); % filter forcemat data
+[valleys(:,1), ind(:,1)]= findpeaks(-signal,'MinPeakHeight',-350,'MinPeakWidth',1,'MinPeakDistance',100);
+valleys= -valleys; % find valley values for forcemats (magnitude during swing)
+[peaks(:,1),~]= findpeaks(signal,'MinPeakHeight',3000,'MinPeakWidth',1,'MinPeakDistance',50);
+threshold= (mean(peaks)/1.1-mean(valleys))*.05 + mean(valleys); % finding rough 5% BW threshold
 for i= 1:length(ind)
-    temp= ind(i);
-    while signal(temp)<threshold && temp < frame_total
-        temp= temp+1;
+    temp_hs= ind(i); % temporary heel strike variable
+    temp_to= ind(i); % temporary toe off variable
+    while signal(temp_hs)<threshold && temp_hs < frame_total % edge case for end of experiment
+        temp_hs= temp_hs+1; % if the value isn't above the threshold move forward 1 frame
     end
-    lhs(i,1)= temp;
+    while signal(temp_to)<threshold && temp_to > 0 % end case for beginning of experiment
+        temp_to= temp_to-1; % if the value isn't above the threshold move backward 1 frame
+    end
+    lhs(i,1)= temp_hs;
+    lto(i,1)= temp_to;
 end
 
-clear ind rhs_fm
-signal= smooth(force_right,10);
-[valleys(:,1), ind(:,1)]= findpeaks(-signal,'MinPeakHeight',-350,'MinPeakWidth',0.65,'MinPeakDistance',100);
-valleys= -valleys;
-[peaks(:,1),~]= findpeaks(signal,'MinPeakHeight',3000,'MinPeakWidth',0.65,'MinPeakDistance',100);
-threshold= (mean(peaks)/1.1-mean(valleys))*.05 + mean(valleys); % finding 5% threshold
+% Right heel strike & toe off using forcemats 
+clear ind peaks valleys
+signal= smooth(force_right,10); % filter forcemat data
+[valleys(:,1), ind(:,1)]= findpeaks(-signal,'MinPeakHeight',-350,'MinPeakWidth',1,'MinPeakDistance',100);
+valleys= -valleys; % find valley values for forcemats (magnitude during swing)
+[peaks(:,1),~]= findpeaks(signal,'MinPeakHeight',3000,'MinPeakWidth',1,'MinPeakDistance',50);
+threshold= (mean(peaks)/1.1-mean(valleys))*.05 + mean(valleys); % finding rough 5% BW threshold
 for i= 1:length(ind)
-    temp= ind(i);
-    while signal(temp)<threshold && temp < frame_total
-        temp= temp+1;
+    temp_hs= ind(i); % temporary heel strike variable
+    temp_to= ind(i); % temporary toe off variable
+    while signal(temp_hs)<threshold && temp_hs < frame_total % edge case for end of experiment
+        temp_hs= temp_hs+1; % if the value isn't above the threshold move forward 1 frame
     end
-    rhs(i,1)= temp;
+    while signal(temp_to)<threshold && temp_to > 0 % end case for beginning of experiment
+        temp_to= temp_to-1; % if the value isn't above the threshold move backward 1 frame
+    end
+    rhs(i,1)= temp_hs;
+    rto(i,1)= temp_to;
 end
 
-% Toe off using Forcemats
-
-%% Toe Off Detection
-[~,lto(:,1)]= findpeaks(-data.ltoe_y,'MinPeakWidth',20,'MinPeakDistance',30);
-[~,rto(:,1)]= findpeaks(-data.rtoe_y,'MinPeakWidth',20,'MinPeakDistance',30);
+% Correct for edge case of beginning or end of experiment
+if lto(1)== 1
+    lto(1)= [];
+end
+if rto(1)== 1
+    rto(1)= [];
+end
+if lhs(end) == frame_total
+    lhs(end)= [];
+end
+if rhs(end) == frame_total
+    rhs(end)= [];
+end
 %% Determining Contact (Stance & Swing)
 
 % Calculate left contact
-if lhs(1)>lto(1) % check if the left leg starts in stance or swing
+if lhs(1)>lto(1) % starts in stance
     hs_counter = 1;
-else
+    lcontact= ones(frame_total,1); % preallocate
+else % starts in swing
     hs_counter = 0;
+    lcontact= zeros(frame_total,1); % preallocate
 end
 to_counter = 1;
-state= 1; % set a starting state (will be changed)
-lcontact= zeros(frame_total,1); % preallocate
 for i= 2:frame_total
     if any(lhs==i)
         hs_counter = hs_counter + 1;
@@ -84,26 +98,25 @@ for i= 2:frame_total
     end
     
     if hs_counter == to_counter
-        state = 1;
+        state = 1; % stance detected
     elseif to_counter == hs_counter + 1
-        state = 0;
+        state = 0; % swing detected
     else
-        'error: heel strike & toe off'
+        error('Error in left contact calculations. Heel strike & toe off out of sync.')
         break
     end
     lcontact(i,1) = state;
 end
-lcontact(1)= lcontact(2); % correct first value
 
 % Calculate right contact
-if rhs(1)>rto(1) % check if the right leg starts in stance or swing
+if rhs(1)>rto(1) % starts in stance
     hs_counter = 1;
-else
+    rcontact= ones(frame_total,1); % preallocate
+else % starts in swing
     hs_counter = 0;
+    rcontact= zeros(frame_total,1); % preallocate
 end
 to_counter = 1;
-state= 0; % set a starting state (will be changed)
-rcontact= zeros(frame_total,1); % preallocate
 for i= 2:frame_total
     if any(rhs==i)
         hs_counter = hs_counter + 1;
@@ -113,18 +126,15 @@ for i= 2:frame_total
     end
     
     if hs_counter == to_counter
-        state = 1;
+        state = 1; % stance detected
     elseif to_counter == hs_counter + 1
-        state = 0;
+        state = 0; % swing detected
     else
-        'error: heel strike & toe off'
+        error('Error in right contact calculations. Heel strike & toe off out of sync.')
         break
     end
     rcontact(i,1) = state;
 end
-rcontact(1)= rcontact(2); % correct first value
-
-data= addvars(data,lcontact,rcontact,'NewVariableNames',{'lcontact','rcontact'});
 
 %% Process Forcemats & Add to data
 % Translation constants
@@ -136,11 +146,11 @@ y_2_offset= -57.55; % y offset of right force mat origin in Vicon frame
 
 % filter left forcemat data
 for i= 1:frame_total
-    if data.lcontact(i)==0 % if in swing set all values to 0
+    if lcontact(i)==0 % if in swing set all values to 0
         fm_grid_left{i} = zeros(96,44);
     else % if in stance set all values far from the foot to 0
-        x_limits= [min([data.lank_x(i) data.ltoe_x(i)])-50 max([data.lhee_x(i) data.ltoe_x(i)])+75];
-        y_limits= [data.lhee_y(i)-10 data.ltoe_y(i)+100];
+        x_limits= [min([lank_x(i) ltoe_x(i)])-50 max([lhee_x(i) ltoe_x(i)])+75];
+        y_limits= [lhee_y(i)-10 ltoe_y(i)+100];
         x_limits= round((x_limits - x_1_offset) / c + 1);
         y_limits= round((y_limits - y_1_offset) / c + 1);
         fm_grid_left{i}(:,1:x_limits(1)) = 0; % all cells too far left
@@ -152,17 +162,14 @@ end
 
 % filter right forcemat data
 for i= 1:frame_total
-    if data.rcontact(i)==0 % if in swing set all values to 0
+    if rcontact(i)==0 % if in swing set all values to 0
         fm_grid_right{i} = zeros(96,44);
     else % if in stance set all values far from the foot to 0
-        x_limits= [min([data.rhee_x(i) data.rtoe_x(i)])-75 max([data.rank_x(i) data.rtoe_x(i)])+50];
-        y_limits= [data.rhee_y(i)-10 data.rtoe_y(i)+75];
-        if x_limits(1)<x_2_offset
-            x_limits(1)= x_2_offset;
-        end
+        x_limits= [min([rank_x(i) rtoe_x(i)])-50 max([rhee_x(i) rtoe_x(i)])+75];
+        y_limits= [rhee_y(i)-10 rtoe_y(i)+100];
         x_limits= round((x_limits - x_2_offset) / c + 1);
         y_limits= round((y_limits - y_2_offset) / c + 1);
-        fm_grid_right{i}(:,1:x_limits(1)) = 0; % all cells too far left
+        fm_grid_right{i}(:,1:x_limits(1)) = 0; % all cells too far right
         fm_grid_right{i}(:,x_limits(2):end) = 0; % all cells too far right
         fm_grid_right{i}(1:y_limits(1),:) = 0; % all cells too far backward
         fm_grid_right{i}(y_limits(2):end,:) = 0; % all cells too far forward
@@ -170,25 +177,10 @@ for i= 1:frame_total
 end
 %% Scale Forcemat Data
 
-weight= 88.45*9.81; % subject weight in N
-
-% Recalculate "force" on left and right
+% Recalculate "force" on left and right (accurate just not scaled)
 for i= 1:length(fm_grid)
     force_left(i,1)= sum(fm_grid_left{i},'all');
     force_right(i,1)= sum(fm_grid_right{i},'all');
-end
-
-% Recalculate peak values
-[left_force_peaks,~]= findpeaks(smooth(force_left),'MinPeakHeight',3000,'MinPeakWidth',0.65,'MinPeakDistance',100);
-[right_force_peaks,~]= findpeaks((force_right),'MinPeakHeight',3000,'MinPeakWidth',0.65,'MinPeakDistance',100);
-
-mean_force_peak= mean([left_force_peaks; right_force_peaks]); % mean between left and right
-C= weight/mean_force_peak; % scaling factor
-
-% Scale forcemat data
-for i= 1:frame_total
-    fm_grid_left{i}= fm_grid_left{i}*C;
-    fm_grid_right{i}= fm_grid_right{i}*C;
 end
 
 % Recombine left and right into single grid 
@@ -246,14 +238,21 @@ for t= 1:frame_total
     fm_force_right(t,1) = total_force_right; % total force on right side
 end
 
-% Add CoP to data
-fm_add= [COP_x COP_y fm_force COP_x_left COP_y_left fm_force_left COP_x_right COP_y_right fm_force_right];
-data= addvars(data,fm_add(:,1),fm_add(:,2),fm_add(:,3),fm_add(:,4),fm_add(:,5),fm_add(:,6),fm_add(:,7),fm_add(:,8),fm_add(:,9),'NewVariableNames',{'cop_x','cop_y','force','cop_x_left','cop_y_left','force_left','cop_x_right','cop_y_right','force_right'});
-clear cop_x cop_y total_force COP_x COP_y fm_force fm_raw fm_grid_left fm_grid_right fm_data_upsampled fm_data
-
-
-
-
-
-
+%% Set Outputs
+fm_data_processed= fm_grid;
+fm_parameters.lcontact= lcontact;
+fm_parameters.rcontact= rcontact;
+fm_parameters.lhs= lhs;
+fm_parameters.rhs= rhs;
+fm_parameters.lto= lto;
+fm_parameters.rto= rto;
+fm_parameters.cop_x= COP_x;
+fm_parameters.cop_y= COP_y;
+fm_parameters.total_force= fm_force;
+fm_parameters.cop_x_left= COP_x_left;
+fm_parameters.cop_y_left= COP_y_left;
+fm_parameters.cop_x_rihgt= COP_x_right;
+fm_parameters.cop_y_right= COP_y_right;
+fm_parameters.force_left= fm_force_left;
+fm_parameters.force_right= fm_force_right;
 end
